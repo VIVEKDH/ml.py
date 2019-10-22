4da26381a05e9efc554a477c24662c48f60f28f7
VIVEK DHAKED <38081924+VIVEKDH@users.noreply.github.com>
2019-10-23 01:30:50 +0530

    Create mll.py

---
+++ mll.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+from scipy import misc
+import cv2
+import numpy as np
+import facenet
+import detect_face
+import os
+import time
+import pickle
+import sys
+
+
+with tf.Graph().as_default():
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.6)
+    sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
+    with sess.as_default():
+        pnet, rnet, onet = detect_face.create_mtcnn(sess, npy)
+
+      
+        
+        HumanNames = os.listdir(train_img)
+        HumanNames.sort()
+
+        print('Loading feature extraction model')
+        facenet.load_model(modeldir)
+
+
+        classifier_filename_exp = os.path.expanduser(classifier_filename)
+        with open(classifier_filename_exp, 'rb') as infile:
+            (model, class_names) = pickle.load(infile)
+
+       
+        c = 0
+
+
+        print('Start Recognition!')
+        prevTime = 0
+    
+        frame = cv2.imread(img_path,0)
